6558c24ffce130fbe6c460363a5ea85a0ad24fb8
Amanda Panell <mpanell@nnu.edu>
2018-10-04 13:07:52 -0600

    Created Java project for labeling images

---
+++ MessageLabeler/src/GUI.java
@@ -0,0 +1,10 @@
+public class GUI {
+    import javax.swing.*;
+    import javax.swing.JFrame;
+    import java.awt.*;
+
+
+    GUI(){
+
+    }
+}
---
+++ MessageLabeler/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args){
+        //Need to create/Update JSON
+        //Need to label messages
+
+
+        //Generate Graphical User Interface
+        GUI labelMachine = new GUI();
+    }
+}
