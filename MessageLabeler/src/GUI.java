ff216e7391929b1a8ccf36495b4dd5cb100764e4
Amanda Panell <mpanell@nnu.edu>
2018-10-05 16:02:07 -0600

    UI changes

--- MessageLabeler/src/GUI.java
+++ MessageLabeler/src/GUI.java
@@ -4,6 +4,8 @@ import javax.swing.JFrame;
 import java.awt.*;
 
 //Needed for ArrayList
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 
 public class GUI {
@@ -24,19 +26,43 @@ public class GUI {
         labelButtonPanel.add(workRelatedButton);
         labelButtonPanel.add(semiWorkRelatedButton);
         labelButtonPanel.add(notWorkRelatedButton);
-
         skipButtonPanel.add(skipButton);
+        messagePanel.setLayout(new CardLayout());
         messagePanel.add(message);
+
+        skipButton.addActionListener(new ActionListener(){
+            @Override
+                    public void actionPerformed(ActionEvent e){
+
+            }
+        });
+        workRelatedButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });
+        semiWorkRelatedButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });
+        notWorkRelatedButton.addActionListener(new ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e){
+
+            }
+        });
+
 
         window.setFocusable(true);
         window.setLayout(new BorderLayout());
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //Exit the program when the exit button is pressed
         window.setTitle("Message Labeling Interface");
-
         window.add(labelButtonPanel, BorderLayout.SOUTH);
         window.add(messagePanel, BorderLayout.CENTER);
         window.add(skipButtonPanel, BorderLayout.NORTH);
-
         window.pack();
         window.setVisible(true);
     }
