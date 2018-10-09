e2868ea2e150af449beb99e3f6287cc644d1b861
Amanda Panell <mpanell@nnu.edu>
2018-10-09 09:22:16 -0600

    Added JAR file for labeling interfact

--- MessageLabeler/src/Main.java
+++ MessageLabeler/src/Main.java
@@ -1,5 +1,6 @@
 import java.io.BufferedReader;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.InputStreamReader;
 import java.nio.file.Files; //Needed to read from/write to files
 import java.nio.file.Paths; //Needed to read from/write to files
@@ -7,9 +8,14 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args){
+        //Generate Graphical User Interface
+        GUI labelMachine = new GUI(readFile("messages.txt"));
+    }
+
+    public static ArrayList readFile(String filename){
         ArrayList<Message> slackMessages = new ArrayList<>();
         try{
-            FileInputStream messageList = new FileInputStream("messages.txt");
+            FileInputStream messageList = new FileInputStream(filename);
             BufferedReader buffRead = new BufferedReader(new InputStreamReader(messageList));
             String newLine;
             while((newLine = buffRead.readLine()) != null){
@@ -17,11 +23,13 @@ public class Main {
                 slackMessages.add(newMessage);
             }
         }
+        catch (FileNotFoundException e404){
+            return readFile("MessageLabeler/messages.txt");
+        }
         catch (Exception e){
             System.out.println("Something went wrong when trying to read messages.txt");
             System.out.println(e.getMessage());
         }
-        //Generate Graphical User Interface
-        GUI labelMachine = new GUI(slackMessages);
+        return slackMessages;
     }
 }
